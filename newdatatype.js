1c71f011b6e2e500565b5ae3cff71e4734776300
JesikaGedam <jesikagedam@gmail.com>
2026-04-26 22:23:33 +0530

    Add files via upload
    Day 1 JavascriptDatatypes

---
+++ newdatatype.js
@@ -0,0 +1,17 @@
+
+
+let firstname="jesika"
+console.log(firstname)
+console.log(typeof(firstname))
+let companyname="Jio"
+console.log(companyname)
+console.log(typeof(companyname))
+let mobilenumber=7030363311
+console.log(mobilenumber)
+console.log(typeof(mobilenumber))
+let Automation=true
+console.log(Automation)
+console.log(typeof(Automation))
+let hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof(hasPlaywright))
